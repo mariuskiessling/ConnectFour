4c2bde9db73545595bb8583178a93af4fced1826
Marius Kießling <github@mariuskiessling.de>
2017-06-22 23:05:37 +0200

    Add basic routes

--- Server/src/Controllers/LoginController.php
+++ Server/src/Controllers/LoginController.php
@@ -3,7 +3,25 @@
 class LoginController {
     public function __construct() {}
 
+    public function showLogin()
+    {
+    }
+
     public function login()
     {
+        if(isset($_POST['identifier']) && isset($_POST['password']))
+        {
+            // Check if user tries to login using an email address or an username
+            if(filter_var($_POST['identifier'], FILTER_VALIDATE_EMAIL))
+            {
+                // Email login
+            } else
+            {
+                // Username login
+            }
+        } else
+        {
+            
+        }
     }
 }
--- Server/src/routes.php
+++ Server/src/routes.php
@@ -2,4 +2,11 @@
 
 use \ConnectFour\Helpers\Router as Router;
 
-Router::Request('GET', '/hello', 'LoginController@sayHello', ['name' => 'Marius']);
+Router::Request('GET', '/login', 'LoginController@showLogin');              // Called to show login
+Router::Request('POST', '/login', 'LoginController@login');                 // Called to validate login
+
+Router::Request('GET', '/register', 'LoginController@showRegistration');    // Called to show registration
+Router::Request('POST', '/register', 'LoginController@register');           // Called to register a new user
+
+Router::Request('GET', '/match', 'MatchController@showMatch');              // Called to show game ($_GET['id'] required)
+Router::Request('GET', '/match/create', 'MatchController@showCreateMatch'); // Callled to create a new game
